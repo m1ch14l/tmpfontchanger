32b89216b21d9f80857c81bd8f51391724f39882
michielhellinx <michiel.hellinx@student.kdg.be>
2016-07-10 17:36:53 +0200

    V1
    V1 of TMPFontChanger

---
+++ TMPFontChanger/TMPFontChanger/Form1.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMPFontChanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkFolder(txt_TMPlocation.Text);
+            LoadFonts();
+        }
+
+        private void LoadFonts()
+        {
+            InstalledFontCollection installedFonts = new InstalledFontCollection();
+            foreach (FontFamily fontFamily in installedFonts.Families)
+            {
+                cb_InstalledFonts.Items.Add(fontFamily.Name);
+            }
+
+            cb_InstalledFonts.SelectedIndex = 0;
+        }
+
+        private void cb_InstalledFonts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lbl_preview.Font = new Font(cb_InstalledFonts.SelectedItem.ToString(), this.lbl_preview.Font.Size);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            string destinationFolder = @"E:\TruckersMP\data\shared\fonts";
+
+            if (File.Exists(destinationFolder + "\\OpenSans.ttf"))
+            {
+                if (File.Exists(destinationFolder + "\\ORIGINAL_OpenSans.ttf"))
+                {
+                    File.Delete(destinationFolder + "\\OpenSans.ttf");
+                }
+                else
+                {
+                    File.Move(destinationFolder + "\\OpenSans.ttf", destinationFolder + "\\ORIGINAL_OpenSans.ttf");
+                }
+            }
+
+            string font = GetSystemFontFileName(new Font(cb_InstalledFonts.SelectedItem.ToString(), this.lbl_preview.Font.Size));
+            string source = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), font);
+            string destination = Path.Combine(destinationFolder, font);
+
+            File.Copy(source, destination);
+            File.Move(destinationFolder + "\\" + font, destinationFolder + "\\OpenSans.ttf");
+        }
+
+        public static string GetSystemFontFileName(Font font)
+        {
+            RegistryKey fonts = null;
+            try
+            {
+                fonts = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion\Fonts", false);
+                if (fonts == null)
+                {
+                    fonts = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Fonts", false);
+                    if (fonts == null)
+                    {
+                        throw new Exception("Can't find font registry database.");
+                    }
+                }
+                string[] names = fonts.GetValueNames();
+                StringBuilder nameb = new StringBuilder(font.Name);
+                if (font.Bold)
+                    nameb.Append(" Bold");
+                if (font.Italic)
+                    nameb.Append(" Italic");
+                nameb.Append(" (TrueType)");
+                string fullname = nameb.ToString();
+                string basename = font.Name + " (TrueType)";
+                object file = fonts.GetValue(fullname);
+                if (file == null && fullname != basename)
+                {
+                    file = fonts.GetValue(basename);
+                }
+                if (file != null) return file.ToString();
+                return null;
+            }
+            finally
+            {
+                if (fonts != null)
+                {
+                    fonts.Dispose();
+                }
+            }
+        }
+
+        private void btnLocation_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                checkFolder(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void checkFolder(string location)
+        {
+            if (!Directory.Exists(location + "\\data"))
+            {
+                txt_TMPlocation.Text = "WRONG FOLDER, CHECK AGAIN!";
+                txt_TMPlocation.ForeColor = Color.Red;
+
+                btn_copy.Enabled = false;
+            }
+            else
+            {
+                txt_TMPlocation.Text = location;
+                txt_TMPlocation.ForeColor = Color.Black;
+                btn_copy.Enabled = true;
+            }
+        }
+    }
+}
---
+++ TMPFontChanger/TMPFontChanger/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TMPFontChanger")]
+[assembly: AssemblyDescription("Small tool to change the fonts in TruckersMP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TMPFontChanger")]
+[assembly: AssemblyCopyright("Copyright ©  2016 - Scarface0359")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0f361dc-97ef-4ebc-9e17-71b96320cc3a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
